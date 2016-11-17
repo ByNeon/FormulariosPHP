c373a645f888bf5a8fdf58ba5b70d1f933bd232e
Alex Lopez <alexlopezbox@gmail.com>
2016-11-17 16:47:25 +0100

    formulario html creado

--- index.php
+++ index.php
@@ -8,8 +8,19 @@ Formulario mas completo
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form action="datos.php" method="POST">
+            Nombre: <input type="text" name="nombre">
+            Edas: <input type="number" name="edad">
+            Ciudad: <select name="ciudad">
+                <option>Barcelona</option>
+                <option>Girona</option>
+                <option>Lleida</option>
+                <option>Tarragona</option>
+            </select>
+            Sexo: 
+            Hombre: <input type="radio" name="sexo" value="hombre">
+            Mujer: <input type="radio" name="sexo" value="mujeer">
+            <input type="submit" name="enviar" value="Enviar Datos">
+        </form>
     </body>
 </html>
